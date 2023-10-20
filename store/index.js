69ce0bd2c33834da95facd5835d9f7419fbb208e
DOMAA21 <clashecurry21@gmail.com>
2023-10-20 19:21:38 +0800

    loginFunctionssss

--- store/index.js
+++ store/index.js
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
 import { persistStore, persistReducer } from 'redux-persist';
-import persistConfig from './reduxPersist'; // Import the Redux Persist config
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const persistConfig = {
+  key: 'auth', 
+  storage: AsyncStorage, 
+  whitelist: ['auth'], 
+};
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 const store = configureStore({
   reducer: persistedReducer, 
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
--- store/reduxPersist.js
+++
@@ -1,9 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-const persistConfig = {
-  key: 'auth', 
-  storage: AsyncStorage, 
-  whitelist: ['auth'], 
-};
-
-export default persistConfig;
