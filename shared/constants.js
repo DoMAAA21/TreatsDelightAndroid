96dd317390c114c54196bcecccb0085dfc9090f7
DoMAAA21 <clashecurry21@gmail.com>
2024-03-12 20:56:06 +0800

    add server

--- shared/constants.js
+++ shared/constants.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = 'http://192.168.254.142:4000';
+export const BACKEND_URL = 'https://treatsdelight-server-gwvt3.ondigitalocean.app';
 export const WEB_URL = 'treatsdelight.vercel.app';
 
 
